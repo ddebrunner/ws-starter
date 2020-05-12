9ca6b5d33cd987dca6295314436e04b3888ad29c
Dan Debrunner <ddebrunner@users.noreply.github.com>
2020-05-12 13:59:35 -0700

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ const NotFoundPage = () => (
   <div>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness, here's an image to cheer you up.</p>
-    <img src="https://picsum.photos/200/300" alt="Random image" width="200" height="300">
+    <img src="https://picsum.photos/200/300" alt="Random image" width="200" height="300"/>
   </div>
 )
 
