ce2637b0fc0f53ffc5690ef561dcc62675bcf257
Dan Debrunner <dan@stepzen.com>
2020-08-21 16:48:19 -0700

    Add GQL

---
+++ src/functions/gql.js
@@ -0,0 +1,24 @@
+exports.handler = async (event, context) => {
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+return fetch(process.env.SZ_DM_URL, {
+    headers: {
+      "content-type": "application/json"
+      "Authorization": "apikey " + process.env.SZ_API_KEY
+    },
+    method: "POST",
+    body: event.body
+  })
+  .then(response => response.json())
+  .then(data => ({
+    statusCode: 200,
+    body: data
+    }))
+    .catch(error => ({
+      statusCode: 422,
+      body: `Oops! Something went wrong. ${error}`
+    }));
+};
