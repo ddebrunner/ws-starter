e9b43105227f584905a20e64fe4bc7f1dd434e5d
Dan Debrunner <dan@stepzen.com>
2020-08-21 16:58:46 -0700

    Fix function

--- src/functions/gql.js
+++ src/functions/gql.js
@@ -1,24 +1,27 @@
 exports.handler = async (event, context) => {
   // Only allow POST
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' }
   }
 
-return fetch(process.env.SZ_DM_URL, {
+  console.log('URL')
+  console.log(process.env.SZ_DM_URL)
+
+  return fetch(process.env.SZ_DM_URL, {
     headers: {
-      "content-type": "application/json"
-      "Authorization": "apikey " + process.env.SZ_API_KEY
+      'content-type': 'application/json',
+      Authorization: 'apikey ' + process.env.SZ_API_KEY,
     },
-    method: "POST",
-    body: event.body
+    method: 'POST',
+    body: event.body,
   })
-  .then(response => response.json())
-  .then(data => ({
-    statusCode: 200,
-    body: data
+    .then((response) => response.json())
+    .then((data) => ({
+      statusCode: 200,
+      body: data,
     }))
-    .catch(error => ({
+    .catch((error) => ({
       statusCode: 422,
-      body: `Oops! Something went wrong. ${error}`
-    }));
-};
+      body: `Oops! Something went wrong. ${error}`,
+    }))
+}
